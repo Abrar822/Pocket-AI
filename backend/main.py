bb949abf82f505c900db4ca53b9db7b7b215224f
Abrar <abrarshekh822@gmail.com>
2026-08-19 18:02:44 +0530

    Modified prompt endpoint and implemented memory module part-1

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,5 @@
-from .pydantic_models.task_router_models import TaskRouterResponse
-from .pydantic_models.llm_models.llm_models import LLMRequestModel
-from .pocket_ai_modules.text_to_speech_module.Piper_TTS import tts
-from .core.TaskRouter import TaskRouter
-from fastapi import FastAPI
-from .core.llm import route_task
+# Endpoint to generate the response from llm after receiving the prompt
+# Run Qwen:=> .\backend\core\llama_cpp\llama-server.exe -m ".\backend\core\model\qwen2.5-1.5b-instruct-q4_k_m.gguf" -c 4096
 
 # data = {
 #     "response": "Email is being generated, pls dont press any key sir",
@@ -17,25 +13,26 @@ from .core.llm import route_task
 #     ],
 # }
 
+from fastapi.middleware.cors import CORSMiddleware
+from .pocket_ai_modules.text_to_speech_module.Piper_TTS import tts
+from .core.TaskRouter import TaskRouter
+from fastapi import FastAPI
+from .pocket_ai_modules.persistent_memory.db import db
+from .core.llm_prompt_endpoint import llm_prompt_router
+from .pocket_ai_modules.persistent_memory.memory_endpoints import memory_endpoints
+
 app = FastAPI()
-speaker = tts.TextToSpeechModule()
 ai = TaskRouter()
+speaker = tts.TextToSpeechModule()
+db()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# Endpoint to generate the response from llm after receiving the prompt
-# Run Qwen:=> .\backend\core\llama_cpp\llama-server.exe -m ".\backend\core\model\qwen2.5-1.5b-instruct-q4_k_m.gguf" -c 4096
-@app.post("/prompt")
-def generate_response(request: LLMRequestModel):
-    try:
-        data = route_task(request.prompt)
-        print("Data returned by llm", data)
-
-        data = TaskRouterResponse.model_validate(data)
-
-        speaker.tts(data.response)
-
-        ai.execute(data.tasks)
-
-    except Exception as err:
-        speaker.tts("Sorry I cannot help with that")
-        print(str(err))
+app.include_router(llm_prompt_router)
+app.include_router(memory_endpoints)
