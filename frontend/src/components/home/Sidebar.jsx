d502f65cb6c61cf78beb051ed0b1d276a8b1b2dc
Chahat Vora <chahatvora1008@gmail.com>
2026-09-17 10:52:34 +0530

    setting page updated

--- frontend/src/components/home/Sidebar.jsx
+++ frontend/src/components/home/Sidebar.jsx
@@ -7,7 +7,7 @@ export default function Sidebar({collapsed,onDashboardClick,onSettingClick}) {
     <div className={`side-bar ${collapsed ? 'collapsed' : ''}`}>
         <Sidebar_Compo onClick={onDashboardClick} compo={'dashboard'} collapsed={collapsed}/>
         <Sidebar_Compo onClick={onDashboardClick}compo={'memory'} collapsed={collapsed}/>
-        <Sidebar_Compo onClick={onSettingClick}compo={'setting'} collapsed={collapsed}/>
+        <Sidebar_Compo onClick={onSettingClick} compo={'setting'} collapsed={collapsed}/>
     </div>
     )
 }
